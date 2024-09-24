7f5bc004c6c56bee181ac0681463a0789916e768
Choi, Inseok <abcdlstjr@gmail.com>
2024-09-25 02:10:41 +0900

    sixth

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main{
     public static void main(String[] args){
         System.out.println("최인석");
         System.out.println("하나");
+        System.out.println("둘");
     }
-    
+
 }
